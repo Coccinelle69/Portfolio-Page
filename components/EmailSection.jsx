cf5dd1011a329389f9661e6e275abf8840661d0c
Your name <Your email>
2024-04-20 01:06:15 +0200

    z-index

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -98,12 +98,12 @@ const EmailSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
+      <div className="z-0">
         <h5 className="text-xl font-bold text-white my-2">
           {t("connectTitle")}
         </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">{t("connectText")}</p>
-        <div className="socials flex flex-row gap-2">
+        <p className="text-[#ADB7BE] mb-4 max-w-md ">{t("connectText")}</p>
+        <div className="flex flex-row gap-2">
           <Image src={GithubIcon} alt="Github Icon" />
         </div>
       </div>
