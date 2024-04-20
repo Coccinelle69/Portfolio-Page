c74223651e57c8501c25a9bbed607e362968671b
Your name <Your email>
2024-04-20 12:40:36 +0200

    cursor

--- app/page.js
+++ app/page.js
@@ -9,15 +9,38 @@ import HeroSection from "@/components/HeroSection";
 import NavBar from "@/components/NavBar";
 import ProjectsSection from "@/components/ProjectsSection";
 import AnimCursor from "@/components/AnimCursor";
+import { useState, useEffect } from "react";
 
 const store = configureStore({
   reducer: { language },
 });
 
 export default function Home() {
+  const [mobileWidth, setMobileWidth] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setMobileWidth(true);
+      } else {
+        setMobileWidth(false);
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+    }
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
+  }, [window.innerWidth]);
   return (
     <Provider store={store}>
-      <AnimCursor />
+      {!mobileWidth && <AnimCursor />}
       <main className="flex min-h-screen flex-col bg-[#221c25] ">
         <NavBar />
         <div className="container mt-24 mx-auto px-12 py-4">
